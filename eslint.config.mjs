2de5158f3bb58a6bf418322c588969d840984ee0
KTCL13 <cesard20101@gmail.com>
2025-10-14 14:28:03 -0500

    se agrega la configuracion inicial para CI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 // eslint.config.mjs
 import js from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin-js';
+import stylistic from '@stylistic/eslint-plugin';
 import globals from 'globals';
+import pluginJest from 'eslint-plugin-jest';
 import prettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 
@@ -10,10 +11,12 @@ export default defineConfig({
   languageOptions: {
     globals: {
       ...globals.node,
+      ...globals.jest,
     },
   },
   plugins: {
     '@stylistic': stylistic,
+    jest: pluginJest,
   },
   extends: [js.configs.recommended, prettier],
   rules: {
