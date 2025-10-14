a9df4817c7343a7495a091b986a34d2066d5173b
KTCL13 <cesard20101@gmail.com>
2025-10-14 15:21:31 -0500

    Se crea la migración de la tabla users

---
+++ migrations/20251014201539-create-user.cjs
@@ -0,0 +1,26 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      is_active_session: {
+        type: Sequelize.BOOLEAN,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  },
+};
