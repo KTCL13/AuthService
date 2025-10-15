a4107cc19d8aa7e42080c26525591a83c303d4d8
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-10-15 15:19:41 -0500

    Se cambia el modelo Users de CommonJS a Esmodules para correcta integracion con test y futuras implementaciones (#17)
    * Se cambia la ubicacion de la configuaracion de la base de datos a src/config
    
    * Se cambia el modelo Users de CommonJS a Esmodules para correcta integracion.
    
    * Se cambia el modelo Users de CommonJS a Esmodules para correcta integracion.

--- src/models/User.js
+++ src/models/User.js
@@ -1,35 +1,31 @@
-import { DataTypes } from 'sequelize';
-import { define } from '../config/database';
-
-const User = define(
-  'User',
-  {
-    id: {
-      type: DataTypes.BIGINT,
-      primaryKey: true,
-      autoIncrement: true,
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      is_active_session: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
+    {
+      tableName: 'users',
+      timestamps: false,
+    }
+  );
 
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-    is_active_session: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  },
-  {
-    tableName: 'users',
-    timestamps: false,
-  }
-);
-
-export default User;
+  return User;
+};
