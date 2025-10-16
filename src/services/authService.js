bc63f40d2d4291c6a0865aa43aa382c46aa51e47
KTCL13 <cesard20101@gmail.com>
2025-10-16 11:49:26 -0500

    Se refactoriza la funcion login en service login para usar la funcion comparePasswords y errores personalizados

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,15 +1,17 @@
 import { findByEmail } from '../repositories/userRepository.js';
+import { UnauthorizedError, NotFoundError } from '../utils/errors.js';
+import { comparePasswords } from '../utils/passwordUtils.js';
 
 export const login = async (email, password) => {
   const user = await findByEmail(email);
   if (!user) {
-    throw new Error('Usuario no encontrado');
+    throw new NotFoundError('Usuario no encontrado');
   }
 
-  const isPasswordCorrect = password === user.password;
+  const isPasswordCorrect = comparePasswords(password, user.password);
 
   if (!isPasswordCorrect) {
-    throw new Error('Credenciales incorrectas');
+    throw new UnauthorizedError('Credenciales incorrectas');
   }
 
   return user;
