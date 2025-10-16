f37b38327c0710cab99fa7bb17a186bf9be0cbfa
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-10-16 11:46:54 -0500

    Se crea una clase de errores personalizados con errores comunes del servicio (#23)
    * Se crea el controlador para el servicio de login
    
    * Se corrigen los test de authController para que difrencie entre contraseña incorrecta y usuario no encontrado
    
    * Se crea una clase de errores personalizados con errores comunes del servicio
    
    * Se añade una funcion para comparar contraseñas

---
+++ src/utils/errors.js
@@ -0,0 +1,46 @@
+// src/utils/errors.js
+import { STATUS_CODES } from './httpStatusCodes.js';
+
+export class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message = 'Solicitud inválida') {
+    super(message, STATUS_CODES.BAD_REQUEST);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = 'No autorizado') {
+    super(message, STATUS_CODES.UNAUTHORIZED);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message = 'Acceso prohibido') {
+    super(message, STATUS_CODES.FORBIDDEN);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message = 'Recurso no encontrado') {
+    super(message, STATUS_CODES.NOT_FOUND);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message = 'Conflicto de datos') {
+    super(message, STATUS_CODES.CONFLICT);
+  }
+}
+
+export class InternalServerError extends AppError {
+  constructor(message = 'Error interno del servidor') {
+    super(message, STATUS_CODES.INTERNAL_SERVER_ERROR);
+  }
+}
