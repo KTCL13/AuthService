80ea7c7ea7e9685e3e03d56739c725d9cb349b66
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-10-16 12:50:29 -0500

    Se agrega un clase para el manejo de errores de toda la aplicacion errorHandler (#26)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express, { json } from 'express';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 const PORT = process.env.PORT;
 const HTTP_OK = 200;
 
 app.use(json());
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.status(HTTP_OK).json({ message: 'API de autenticación funcionando correctamente.' });
