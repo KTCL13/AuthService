2125148e6668b4a381e3c3596239a72b4750565e
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-10-19 18:20:56 -0500

    Se agrega funcion en la capa de repositorio para cambiar el estado de una sesion de Usuario (changeUserSesionState) (#33)

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -3,3 +3,7 @@ import db from '../models/index.js';
 export const findByEmail = async (email) => {
   return await db.User.findOne({ where: { email } });
 };
+
+export const changeUserSesionState = async (userId, isActive) => {
+  return await db.User.update({ is_active_session: isActive }, { where: { id: userId } });
+};
