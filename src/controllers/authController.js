19a29d49a2a21e5e88b1aa3cc2c2a7fa49aa009f
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-10-19 23:24:17 -0500

    Se agrega la función verifySessionStatus en la capa de controlador para manejar respuestas sobre el estado de la sesión de un usuario y se añaden pruebas correspondientes. (#39)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 // src/controllers/authController.js
-import { login as _login } from '../services/authService.js';
+import { login as _login, validateSessionState } from '../services/authService.js';
 import { STATUS_CODES } from '../utils/httpStatusCodes.js';
 
 export const login = async (req, res, next) => {
@@ -8,6 +8,16 @@ export const login = async (req, res, next) => {
   try {
     const user = await _login(email, password);
     res.status(STATUS_CODES.OK).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const verifySessionStatus = async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    const sessionActive = await validateSessionState(email);
+    res.status(STATUS_CODES.OK).json({ success: true, sessionActive });
   } catch (error) {
     next(error);
   }
