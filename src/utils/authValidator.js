61a8ee05ecf5a3cb4d84d970b1cc4466de47f9d1
Cesar Pérez Fonseca <cesard20101@gmail.com>
2025-10-19 23:25:52 -0500

    Se agrega validacion de correo a authValidator para validar request que solo necesitan un correo

--- src/utils/authValidator.js
+++ src/utils/authValidator.js
@@ -4,3 +4,7 @@ export const loginValidator = [
   body('email').isEmail().withMessage('Por favor, ingrese un email válido'),
   body('password').notEmpty().withMessage('La contraseña es requerida'),
 ];
+
+export const sessionStatusValidator = [
+  body('email').isEmail().withMessage('Por favor, ingrese un email válido'),
+];
