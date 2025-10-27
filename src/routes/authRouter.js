8029397ad3cfe29738f13a8993d2bc55018ac17c
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2025-10-26 21:42:51 -0500

    Se agrega la ruta de logout en authRouter para manejar el cierre de sesión de un usuario. (#44)

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,12 +2,14 @@
 import { Router } from 'express';
 import { loginValidator, sessionStatusValidator } from '../utils/authValidator.js';
 import { validateRequest } from '../middlewares/validateRequest.js';
-import { login, verifySessionStatus } from '../controllers/authController.js';
+import { login, verifySessionStatus, logoutController } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/login', loginValidator, validateRequest, login);
 
 router.post('/session-status', sessionStatusValidator, validateRequest, verifySessionStatus);
+
+router.post('/logout', sessionStatusValidator, validateRequest, logoutController);
 
 export default router;
