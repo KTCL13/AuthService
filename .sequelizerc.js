1a4df4a57fbc73272ba89ff0fc7866807744f629
Cesar Pérez Fonseca <cesard20101@gmail.com>
2025-11-18 23:14:31 -0500

    Replace .sequelizerc.js with .sequelizerc for improved configuration management

--- .sequelizerc.js
+++
@@ -1,8 +0,0 @@
-import { resolve } from 'path';
-
-export default {
-  config: resolve('src', 'config', 'config.js'),
-  'models-path': resolve('src', 'models'),
-  'migrations-path': resolve('migrations'),
-  'seeders-path': resolve('seeders'),
-};
